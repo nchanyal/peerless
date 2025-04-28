d55928f00e2bea95aba048020267248abb70fd7d
nathnael <134805961+nchanyal@users.noreply.github.com>
2025-04-28 16:50:18 -0400

    Add a function to sync users from Clerk to Neon
    Before this commit, users would sign up with Clerk
    but their information wouldn't be stored in Neon
    Postgres. But now the 'syncUser' function ensures
    that every new user will have their information
    available in Neon.

---
+++ src/actions/user.actions.ts
@@ -0,0 +1,42 @@
+"use server";
+
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { prisma } from "@/lib/prisma";
+
+export async function syncUser() {
+  try {
+    const { userId } = await auth();
+    const clerkUser = await currentUser();
+
+    // Quit the function if the user isn't signed in
+    if (!userId) return;
+
+    // Quit the function if the firstName, lastName, or primaryEmailAddress fields don't exist
+    if (
+      !clerkUser?.firstName ||
+      !clerkUser?.lastName ||
+      !clerkUser?.primaryEmailAddress
+    )
+      return;
+
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        clerkId: userId,
+      },
+    });
+
+    // Create a new user in the database if the current user doesn't exist there
+    if (!existingUser) {
+      await prisma.user.create({
+        data: {
+          clerkId: userId,
+          firstName: clerkUser?.firstName,
+          lastName: clerkUser?.lastName,
+          email: clerkUser?.primaryEmailAddress?.emailAddress,
+        },
+      });
+    }
+  } catch (error) {
+    console.log("Error in syncUser", error);
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+import { syncUser } from "@/actions/user.actions";
+
 export default function Home() {
+  syncUser();
+
   return <div>hi mom!</div>;
 }
